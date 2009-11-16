115e5289d951b08bb72c42a31c61d3733efd2d6a
David Hrbáč <david@hrbac.cz>
2009-11-16 12:08:00 +0000

    -F

---
+++ david.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+outdir=${TMPDIR:-$HOME/tmp}
+specfile=$(grep -o '\<[a-zA-Z][-_+a-zA-Z0-9]*\.spec\>' <<<"$@" \
+           || { echo "No spec file" 1>&2; exit 1; } | tail -n 1)
+log="$outdir/rpmbuild-$specfile-$$.log"
+if rpmbuild "$@" &>"$log"; then
+  echo Logfile: "$log"
+  sed 's/^Wrote: //;t;d' "$log"
+#  rm "$log"
+else
+  cat "$log" 1>&2
+  echo Logfile: "$log"
+fi
